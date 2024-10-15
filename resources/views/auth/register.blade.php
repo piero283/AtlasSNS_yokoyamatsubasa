dbda4369d4ce8d5dac6fb695c9520d2ac6d9e384
横山翼 <>
2024-10-15 17:14:53 +0900

    新規ユーザー登録　バリデーション表示　修正済

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,32 +4,51 @@
 <!-- 適切なURLを入力してください -->
 {!! Form::open(['url' => '/register']) !!}
 
-<!-- バリデーションチェックのエラーメッセージを表示させるコード -->
-@if($errors->any())
-  <div class="alert alert-danger">
-    <ul>
-      @foreach($errors->all() as $error)
-      <li>{{ $error }}</li>
-      @endforeach
-    </ul>
-  </div>
-@endif
+
 <section class="register-section">
   <h1 class="form-tittle">新規ユーザー登録</h1>
 
   <div class="form-group">
     {{ Form::label('ユーザー名') }}
     {{ Form::text('username',null,['class' => 'input']) }}
+      @if($errors->has('username'))
+      <div class="alert alert-danger">
+        <ul>
+          @foreach($errors->get('username') as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+      @endif
   </div>
 
   <div class="form-group">
   {{ Form::label('メールアドレス') }}
   {{ Form::text('mail',null,['class' => 'input']) }}
+  @if($errors->has('mail'))
+  <div class="alert alert-danger">
+    <ul>
+      @foreach($errors->get('mail') as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+  @endif
   </div>
 
   <div class="form-group">
   {{ Form::label('パスワード') }}
   {{ Form::password('password',['class' => 'input']) }}
+
+  @if($errors->has('password'))
+  <div class="alert alert-danger">
+    <ul>
+      @foreach($errors->get('password') as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+  @endif
   </div>
 
   <div class="form-group">
